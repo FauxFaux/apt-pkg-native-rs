39d8438d3a7ffebcd1c92a7743212747cd6fa478
Chris West (Faux) <git@goeswhere.com>
2017-07-12 20:01:29 +0100

    bindgen isn't even close to working; std::string is hard

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+mod raw;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    }
+}
---
+++ wrapper.hpp
@@ -0,0 +1,4 @@
+#define APT_8_CLEANER_HEADERS
+#define APT_9_CLEANER_HEADERS
+#define APT_10_CLEANER_HEADERS
+#include <apt-pkg/strutl.h>
