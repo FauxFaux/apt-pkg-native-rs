0126dd522078867b917c10fa8e36c62c94ebcb2b
Chris West (Faux) <git@goeswhere.com>
2017-07-13 13:40:51 +0100

    rename

--- build.rs
+++ build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("cargo:rustc-link-search=apt-c")
+    println!("cargo:rustc-link-search=apt-pkg-c")
 }
--- src/raw.rs
+++ src/raw.rs
@@ -9,7 +9,7 @@ use libc::c_char;
 pub type PCache = *mut c_void;
 pub type PPkgIterator = *mut c_void;
 
-#[link(name = "apt-c")]
+#[link(name = "apt-pkg-c")]
 #[link(name = "apt-pkg")]
 #[link(name = "stdc++")]
 extern {
